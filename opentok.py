b034c20e7928307cf396215ec08dd8e664524fb7
stud <stud@studium>
2020-07-30 12:57:42 +0000

    separate opentok & flask api

--- opentok.py
+++ opentok.py
@@ -2,7 +2,6 @@ import time
 import os
 from configparser import ConfigParser
 from opentok import OpenTok, MediaModes, ArchiveModes, Roles
-from flask import Flask
 
 
 # ============= init =============
@@ -12,9 +11,6 @@ config = ConfigParser()
 config.read('config.ini')
 opentok_api_key = config.get('tokens', 'opentok_api_key')
 opentok_api_secret = config.get('tokens', 'opentok_api_secret')
-
-# init flask app
-app = Flask(__name__)
 
 # init opentok
 opentok = OpenTok(opentok_api_key, opentok_api_secret)
@@ -33,10 +29,3 @@ token = session.generate_token(role=Roles.moderator,
                                expire_time=int(time.time()) + 10,
                                data=u'name=Johnny'
                                initial_layout_class_list=[u'focus'])
-
-
-# ============= flask routes =============
-
-@app.route('/')
-def hello():
-    return "test response"
